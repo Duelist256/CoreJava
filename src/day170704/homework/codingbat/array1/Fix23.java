a679e7df4aca6ea5f27f4c6b9e2fa50e32e09561
Duelist <selykov.iw@yandex.ru>
2017-07-06 00:37:14 +0300

    fix23() rewrited without loops

--- src/day170704/homework/codingbat/array1/Fix23.java
+++ src/day170704/homework/codingbat/array1/Fix23.java
@@ -9,10 +9,12 @@ public class Fix23 {
 
     private static int[] fix23(int[] nums) {
         if (nums.length == 3) {
-            for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == 2 && nums[i + 1] == 3) {
-                    nums[i + 1] = 0;
-                }
+            if ((nums[0] == 2 && nums[1] == 3)) {
+                nums[1] = 0;
+            }
+
+            if ((nums[1] == 2 && nums[2] == 3)) {
+                nums[2] = 0;
             }
         }
         return nums;
