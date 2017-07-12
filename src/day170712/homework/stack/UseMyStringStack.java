6913c5b05b51ff91b252f969e7799dc1c6168198
Duelist <selykov.iw@yandex.ru>
2017-07-12 18:19:38 +0300

    Added tests for push() method

--- src/day170712/homework/stack/UseMyStringStack.java
+++ src/day170712/homework/stack/UseMyStringStack.java
@@ -1,11 +1,18 @@
 package day170712.homework.stack;
 
+import day170624.mytestframework.Asserts;
+
 /**
  * Created by Duelist on 12.07.2017.
  */
 public class UseMyStringStack {
     public static void main(String[] args) {
-        MyStringStack stack1 = new MyStringStack(5);
-        MyStringStack stack2 = new MyStringStack();
+        MyStringStack stack = new MyStringStack(3);
+
+        // testing push()
+        System.out.println(Asserts.assertEquals(stack.push("one"), true));
+        System.out.println(Asserts.assertEquals(stack.push("two"), true));
+        System.out.println(Asserts.assertEquals(stack.push("three"), true));
+        System.out.println(Asserts.assertEquals(stack.push("four"), false));
     }
 }
