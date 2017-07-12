767be42941b084ef97d79b3b91309cd113fdf2b2
Duelist <selykov.iw@yandex.ru>
2017-07-12 17:40:22 +0300

    Added class for MyStringStack and class which use it

---
+++ src/day170712/homework/stack/MyStringStack.java
@@ -0,0 +1,8 @@
+package day170712.homework.stack;
+
+/**
+ * Created by Duelist on 12.07.2017.
+ */
+public class MyStringStack {
+
+}
---
+++ src/day170712/homework/stack/UseMyStringStack.java
@@ -0,0 +1,10 @@
+package day170712.homework.stack;
+
+/**
+ * Created by Duelist on 12.07.2017.
+ */
+public class UseMyStringStack {
+    public static void main(String[] args) {
+
+    }
+}
