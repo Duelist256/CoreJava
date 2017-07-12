c37c5ba53bf8d93330bb0f6b2cfd0f82978c729e
Duelist <selykov.iw@yandex.ru>
2017-07-13 01:12:07 +0300

    Added maxSpan from Array-3

---
+++ src/day170704/homework/codingbat/array3/MaxSpan.java
@@ -0,0 +1,43 @@
+package day170704.homework.codingbat.array3;
+
+/**
+ * Created by Duelist on 13.07.2017.
+ */
+public class MaxSpan {
+
+    private static int maxSpan(int[] nums) {
+        int maxSpan = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int span = 0;
+            int num = nums[i];
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == num) {
+                    span = j - i;
+                }
+            }
+
+            if (span > maxSpan) {
+                maxSpan = span;
+            }
+        }
+
+        if (nums.length > 0) {
+            maxSpan++;
+        }
+
+        return maxSpan;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 1, 1, 3};
+        System.out.println(maxSpan(arr));
+        arr = new int[]{1, 4, 2, 1, 4, 1, 4};
+        System.out.println(maxSpan(arr));
+        arr = new int[]{1, 4, 2, 1, 4, 4, 4};
+        System.out.println(maxSpan(arr));
+        arr = new int[]{3, 9};
+        System.out.println(maxSpan(arr));
+    }
+}
